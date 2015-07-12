b9fa2869552138eb87b929b34d3c5be468a8e001
Xiaowei Tan <xiaoweitan@hotmail.com>
2015-07-11 23:25:15 -0500

    added a folder

---
+++ folder/module2.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 11, 2015
+
+@author: Xiaowei
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
